3ef478232fb2985d69120443486f1bf067ab81a0
Austin Miller <aujmiller_wccnet@MININT-K781HSH.labs.is.wccnet.org>
2024-03-23 17:45:46 -0400

    updates

---
+++ assignments/inClassCardDemo/InClassCardDemo/app/src/main/java/edu/wccnet/mmorgan8/carddemo/MainActivity.kt
@@ -0,0 +1,29 @@
+package edu.wccnet.mmorgan8.carddemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import edu.wccnet.mmorgan8.carddemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        layoutManager = LinearLayoutManager(this)
+        binding.contentMain.recyclerView.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        binding.contentMain.recyclerView.adapter = adapter
+    }
+
+}
